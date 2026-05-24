3d7453b312073744f4ca91d711566637e6445809
Murali <muralisurya2004@gmail.com>
2026-05-24 15:48:45 +0530

    Update scrript.js

--- scrript.js
+++ scrript.js
@@ -8,7 +8,6 @@ function random()
     const red = Math.round(Math.random() * 255)
     const green = Math.round(Math.random() * 255)
     const blue = Math.round(Math.random() * 255)
-
     let na =  `rgb(${red},${green},${blue})`
     body.style.backgroundColor = na;
 
